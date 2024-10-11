aab53b5b8c83c9e83609cd568aa8cdc2707adeb4
hrkjt <168939068+hrkjt@users.noreply.github.com>
2024-10-11 11:09:36 +0900

    Update app.py
    print(dfpt)

--- app.py
+++ app.py
@@ -559,6 +559,7 @@ def get_random_pt():
   random_float = int(random.random()*10)/10
   random_id = df_first[id].unique()[int(len(df_first[id].unique())*random_float)]
   dfpt = df_first[df_first[id] == random_id]
+  print(dfpt)
   nan_rows_count = dfpt.isnull().any(axis=1).sum()
   if nan_rows_count > 0:
     dfpt = get_random_pt()
